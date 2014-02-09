9bfffa5e36cc41d3d346fbb9ce3aecc3fabe9a02
lion-wang <mac@51.62.97.119.broad.wh.hb.dynamic.163data.com.cn>
2014-02-09 19:32:45 +0800

    post数据

--- main/serviceServer.js
+++ main/serviceServer.js
@@ -12,8 +12,31 @@ var server = http.createServer(function (request, response) {
   var pathname = url.parse(request.url).pathname;
   console.log(pathname);
   //var realPath = path.join("assets", pathname);
-  //console.log(realPath);
+  //console.log(request);
+  var pars={};
   
+  console.log('STATUS: ' + request.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(request.headers));
+  console.log('BODY: ' + JSON.stringify(request.body));
+  //获取post数据格式为test=9000
+  request.on('data',function(d){
+      console.log("Post Data is:"+d);
+  })
+  if(request.method=="POST"){
+      console.log("Request is POST!");
+  }
+  else if(request.method=="GET"){
+      var parStr=((request.url).toString()).split("?");
+      if(parStr.length>1){
+          var pStrs= parStr[1].split("&");
+          for(var i in pStrs){
+              var p=pStrs[i],
+                  ps=p.split("=");
+              pars[ps[0]]=pars[ps[1]];
+          }
+      }
+  }
+  console.log(pars);
   
   if(pathname.indexOf(servicePath)>=0){
       var allServices=services.services(),
